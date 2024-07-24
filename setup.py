c9f728aa65e6428d3883c61b1288173febcc7bd3
Samuel Poon <42005529+samuel-poon@users.noreply.github.com>
2024-07-24 16:58:17 -0400

    Fix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='standwithcrypto',
-    version='0.0.0',
+    version='0.0.1',
     description='Pull US politician data from standwithcrypto.org',
     python_requires=">=3.6",
     install_requires=[
