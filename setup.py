5d5723d54bdedac619417d813554e99c53ba3ded
Samuel Poon <42005529+samuel-poon@users.noreply.github.com>
2024-07-24 17:06:36 -0400

    Make README long_description

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(
     name='standwithcrypto',
     version='0.0.1',
     description='Pull US politician data from standwithcrypto.org',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     python_requires=">=3.6",
     install_requires=[
         'requests'
